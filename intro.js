4ea212e5fa60857ad4c60788b4fd77853876ca8f
tomaspc83 <tomaspc83@gmail.com>
2016-01-19 19:09:06 +0100

    .

--- intro.js
+++ intro.js
@@ -1,1 +1,11 @@
-alert("Hola mundo de TOMMY!!!");
+var nombre = prompt("Pon tu nombre");
+var edad = prompt("Pon tu edad");
+
+// Shows a pop up
+//alert(nombre);
+
+// It writes the value of the variable on the web page
+document.write("El nombre es: " + nombre + " y la edad: " + edad);
+
+// It writes the value on the developer console
+console.log(nombre);
